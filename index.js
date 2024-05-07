734401ac053718beb1c327097a87955935a46d18
yducijk <js1fshagari@hotmail.com>
2024-05-08 04:40:32 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const name = "john";
-const upperName = name.toUpperCase();
-console.log(upperName); // JOHN
+function isValidParentheses(s) {
+  const stack = [];
+  const map = { "(": ")", "[": "]", "{": "}" };
+  for (const char of s) {
+    if (char in map) stack.push(char);
+    else {
+      const top = stack.pop();
+      if (map[top] !== char) return false;
+    }
+  }
+  return stack.length === 0;
+}
